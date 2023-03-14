49721c8ef9d8dc76b6338a67fbc231d9e63b8534
Stanislav Pravdiuk <stanislav.pravdiuk@gmail.com>
2023-03-14 12:38:58 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 import './css/styles.css';
+// import countryInfoTpl from '../src/country-info.hbs';
 
 const DEBOUNCE_DELAY = 300;
+const refs = {
+    countryContainer: document.querySelector('.country-info'),
+}
+
+fetch('https://restcountries.com/v3.1/name/ukraine')
+    .then(response => {
+        return response.json()
+    })
+    .then(country => {
+        console.log(country[0].name.common);
+        // const markup = countryInfoTpl(country);
+        // console.log(markup);
+        // refs.countryContainer.innerHTML =
+    })
+    .catch(error => {
+        console.log(error)
+    })
+
