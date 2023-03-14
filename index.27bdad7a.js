7e774dbd894a98f75e525a32715961230b13a5ea
stanislav-pravdiuk <114864904+stanislav-pravdiuk@users.noreply.github.com>
2023-03-14 12:30:15 +0000

    Deploying to gh-pages from @ stanislav-pravdiuk/goit-js-hw-10@9e36a4c3bced314bb5048955edbe1c364e20ad09 🚀

--- index.27bdad7a.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".country-info");fetch("https://restcountries.com/v3.1/name/ukraine").then((o=>o.json())).then((o=>{console.log(o[0].name.common)})).catch((o=>{console.log(o)}));
-//# sourceMappingURL=index.27bdad7a.js.map
