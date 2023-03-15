e1acf367155caa24ad8543da84b7d9be9b0cf1ba
Stanislav Pravdiuk <stanislav.pravdiuk@gmail.com>
2023-03-15 11:07:28 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,29 +1,48 @@
 import './css/styles.css';
 import Notiflix from 'notiflix';
 
-const DEBOUNCE_DELAY = 300;
+// const DEBOUNCE_DELAY = 300;
 const refs = {
     countryContainer: document.querySelector('.country-info'),
-    countryList: document.querySelector('.country-list')
+    countryList: document.querySelector('.country-list'),
+    searchForm: document.querySelector('#search-box')
 };
 
-fetch('https://restcountries.com/v3.1/name/ru')
-    .then(response => {
-        return response.json()
-    })
+refs.searchForm.addEventListener('input', onSearch);
 
+function onSearch(e) { 
+    e.preventDefault();
+
+    const textInput = e.currentTarget.value;
+    console.log(textInput)
+    fetchCountries(textInput)
     .then(renderMarkup)
-
     .catch(error => {
         Notiflix.Notify.failure('Oops, there is no country with that name')
     });
+};
 
-// fetchCountries() {
-    
-// };
+
+
+function fetchCountries(countryName) { 
+    return fetch(`https://restcountries.com/v3.1/name/${countryName}`)
+    .then(response => {
+        return response.json()
+    })
+};
+// fetch('https://restcountries.com/v3.1/name/ru')
+//     .then(response => {
+//         return response.json()
+//     })
+
+//     .then(renderMarkup)
+
+//     .catch(error => {
+//     Notiflix.Notify.failure('Oops, there is no country with that name')
+//     });
 
 function renderMarkup(country) { 
-    if (country.length > 10) {    
+    if (country.length > 10) {
         Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
 
     } else if (country.length >= 2 || country.length > 10) {
@@ -37,8 +56,9 @@ function renderMarkup(country) {
                 </li>
                 `)
         });
-    } else 
-            refs.countryContainer.innerHTML = `
+    } else
+    refs.countryList.innerHTML = '';
+    refs.countryContainer.innerHTML = `
                 <div class="country-info__imgContainer">
                     <img class="country-info__img" src="${country[0].flags.svg}" alt="${country[0].flags.alt}">
                     <h1 class="country-info__name">${country[0].name.official}</h1>
@@ -48,6 +68,6 @@ function renderMarkup(country) {
                     <p class="country-info__text"><b>Population:</b> ${country[0].population}</p>
                     <p class="country-info__text"><b>Languages:</b> ${Object.values(country[0].languages)}</p>
                 </div>
-    `
+    `;
 };
 
