2341eb560785368fed92f8610327560121f18de6
Stanislav Pravdiuk <stanislav.pravdiuk@gmail.com>
2023-03-16 15:19:35 +0200

    add nitify error

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,6 +1,11 @@
 function fetchCountries(countryName) { 
     const url = `https://restcountries.com/v3.1/name/${countryName}`
-    return fetch(url).then(response => response.json()
+    // return fetch(url).then(response => response.json()
+    return fetch(url).then(response => {
+        if (response.ok) {
+            return response.json()
+        } throw new Error(response.statusText)
+    } 
 )
 };
 
