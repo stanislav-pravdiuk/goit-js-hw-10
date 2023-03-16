504146bdd50baa56060408f7daf78135b0c9689e
Stanislav Pravdiuk <stanislav.pravdiuk@gmail.com>
2023-03-16 16:29:49 +0200

    update

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,7 +1,6 @@
 function fetchCountries(countryName) { 
-    const url = `https://restcountries.com/v3.1/name/${countryName}?fields=name,flags,capital,population,languages`;
+    const url = `https://restcountries.com/v3.1/name/${countryName}`;
     return fetch(url).then(response => {
-        console.log(response.json())
         if (response.ok) {
             return response.json()
         } throw new Error(response.statusText)
